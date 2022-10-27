350c181c5b6597780bfe1a19ad4d02d1ddfdc338
Darius Arion <dariusarion01@gmail.com>
2022-10-27 14:33:39 +0300

    --last commit

--- ecommerce/next.config.js
+++ ecommerce/next.config.js
@@ -2,11 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  mode: 'universal',
   images:{
 domains:['localhost'],
   },
   env:{
-    NEXT_PUBLIC_SANITY_TOKEN: process.env.NEXT_PUBLIC_SANITY_TOKEN
+    NEXT_PUBLIC_SANITY_TOKEN: process.env.NEXT_PUBLIC_SANITY_TOKEN,
+    NEXT_PUBLIC_STRIPE_SECRET_KEY: process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY,
+    NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
   }
 }
 
